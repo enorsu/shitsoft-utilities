7bbf17a38e76e854eb879390ff47fba00a72b02c
SotaNorsu <pelaaja538@gmail.com>
2024-05-14 17:55:04 +0300

    rtyfhwsrlökfsrröjfgnrgwgewrg

--- manager.py
+++ manager.py
@@ -3,15 +3,18 @@ import os
 import time
 import sys
 import tkinter.ttk as ttk
-
-
+from PIL import Image
+import threading
 
 
 def loginWindow():
-
+    global root
     root = tk.Tk()
     root.title("")
     root.resizable(False,False)
+
+    global usernameVariable
+    global passwordVariable
 
     usernameVariable = tk.StringVar()
     passwordVariable = tk.StringVar()
@@ -33,12 +36,42 @@ def loginWindow():
     passentry.grid(column=0, row=4)
 
 
-    submitBtn = tk.Button(root, text="login", font=("Arial", 10), cursor="hand2")
+    submitBtn = tk.Button(root, text="login", font=("Arial", 10), cursor="hand2", command=dontReallyLogin)
     submitBtn.grid(column=0, row=5)
 
 
     root.mainloop()
 
+def dontReallyLogin():
+    if usernameVariable.get() == "bob" and passwordVariable.get() == "admin":
+        root.destroy()
+
+        showThread = threading.Thread(target=showImages)
+        showThread1 = threading.Thread(target=showImages1)
+        showThread3 = threading.Thread(target=showImages3)
+
+        showThread.start()
+        showThread1.start()
+        showThread3.start()
+
+
+def showImages():
+    for i in range(10):
+        bob = Image.open("bob.png")
+        bob.show()
+        bob.close()
+
+def showImages1():
+    for i in range(10):
+        bob = Image.open("bob.png")
+        bob.show()
+        bob.close()
+
+def showImages3():
+    for i in range(10):
+        bob = Image.open("bob.png")
+        bob.show()
+        bob.close()
 
 
 if __name__ == "__main__":
